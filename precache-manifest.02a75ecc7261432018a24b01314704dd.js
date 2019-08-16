9c2ea940522880f4a03e1739e5263bf3185bdd13
juansecalvinomtg <juansecalvinomtg@gmail.com>
2019-08-16 13:52:32 -0300

    Updates

---
+++ precache-manifest.02a75ecc7261432018a24b01314704dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453f7487b75d1f9ea3f87cb52e76610a",
+    "url": "/proyecto-news-feed/index.html"
+  },
+  {
+    "revision": "086f85fc1b5cfae74806",
+    "url": "/proyecto-news-feed/static/css/main.35ed09e4.chunk.css"
+  },
+  {
+    "revision": "9155f15ea59c288eee49",
+    "url": "/proyecto-news-feed/static/js/2.e5aa889e.chunk.js"
+  },
+  {
+    "revision": "086f85fc1b5cfae74806",
+    "url": "/proyecto-news-feed/static/js/main.a474e5e3.chunk.js"
+  },
+  {
+    "revision": "de33c760d4c0a35e9709",
+    "url": "/proyecto-news-feed/static/js/runtime~main.4f26c52b.js"
+  }
+]);
