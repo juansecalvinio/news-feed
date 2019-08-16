1a56c86edd1208ecda7ef9677c1ea8d1e9c10846
juansecalvinomtg <juansecalvinomtg@gmail.com>
2019-08-16 13:51:56 -0300

    Commit final to deploy

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 
 import Header from './../Header';
